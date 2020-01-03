5c8974d5eadb7383d0a82d38a563ae536846d699
oleksiislavik <oleksii.slavik@sophos.com>
2020-01-03 15:16:44 +0200

    add floating action button

--- calendar/src/main/java/com/itos/calendar/MainActivity.java
+++ calendar/src/main/java/com/itos/calendar/MainActivity.java
@@ -2,7 +2,6 @@ package com.itos.calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CalendarView calendarView = findViewById(R.id.calendarView);
     }
 }
